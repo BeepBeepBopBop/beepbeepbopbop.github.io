05704fd8e268623d75b193f7da9779cdbe4f277c
BeepBeepBopBop <evan.carrere@gmail.com>
2026-07-09 19:15:11 +0200

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PapilloteWebsite.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Pages/MentionsLegales.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PapilloteWebsite.Pages;
+
+public class MentionsLegalesModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
